817db83fbbc1fd44144fc27c76f61428991850e0
Robert Lin <Rlin415@gmail.com>
2015-10-02 18:05:54 -0700

    Solved getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,31 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ){
-  // your code here
+
+  var result = [];
+  var prev = arguments[1] || document.children[0].children[1];
+  /*if (typeof className !== string) {
+    console.log('Error! Input is not a string.');
+    return;
+  }*/
+
+  if (prev.children.length === 0) {
+    var arrayOfNames = prev.className.split(' ');
+      if (arrayOfNames.indexOf(className) !== -1) {
+        result.push(prev);
+      }
+      return result;
+  }
+
+  for (var i = 0; i < prev.children.length; i++) {
+    var result2 = getElementsByClassName(className, prev.children[i]);
+    if (result2.length > 0) {
+      result = result2.concat(result);
+    }
+  }
+
+  if (prev.className === className) {
+      result.unshift(prev);
+  }
+  return result;
 };
