14703bda7fab31b060750efd79980dd3f2e68ad3
Robert Lin <Rlin415@gmail.com>
2015-10-05 17:54:19 -0700

    Implemented stringify up to arrays

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,27 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var string = '';
+  if (typeof obj === 'number' || obj === true || obj === false || obj === null) {
+    return string.concat(obj);
+  }
+
+  if (Array.isArray(obj)) {
+    string = '[';
+    obj.forEach(function(element, idx) {
+      if (typeof element === 'string') {
+        element = '\"' + element + '\"';
+      } else if (Array.isArray(element)) {
+        element = stringifyJSON(element);
+      }
+      if (obj.length > 1 && idx !== 0) {
+        string = string.concat(',', element);
+      } else {
+        string  = string.concat(element);
+      }
+    });
+    return string + (']');
+  }
+
+  return '\"' + obj + '\"';
 };
